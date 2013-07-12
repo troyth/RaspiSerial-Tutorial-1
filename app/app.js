d7967c18b64a16740e8d1077b42f46a2070105b2
Troy Conrad Therrien <troyth@gmail.com>
2013-07-12 19:34:37 -0400

    upload

---
+++ app/app.js
@@ -0,0 +1,68 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+var five = require("johnny-five"),
+    board, led;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create a standard `led` hardware instance
+  led = new five.Led({
+    pin: 13
+  });
+
+  // "on" turns the led _on_
+  led.on();
+
+  // "off" turns the led _off_
+  led.off();
+
+  // Turn the led back on after 3 seconds (shown in ms)
+  this.wait( 3000, function() {
+
+    led.on();
+    this.wait( 3500, function(){
+    	led.off();
+    })
+
+  });
+});
